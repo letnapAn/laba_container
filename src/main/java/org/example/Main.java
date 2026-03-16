e6f22ca0c28ccd94316b66dfbf9379a9d6871531
Анастасия Пантелеева <2204262@mail.ru>
2026-03-16 19:30:21 +0300

    feat: added tests in main - check minvalue in IntOpenAddressingHashList - check double value in IntOpenAddressingHashList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -33,5 +33,34 @@ public class Main {
         // Очистка
         container.clear();
         System.out.println("Size after clear: " + container.size());
+
+
+
+        IntContainer containerList = new IntOpenAddressingHashList();
+
+        // Добавление элементов
+        containerList.add(1);
+        containerList.add(2);
+        containerList.add(1); // Дубль
+        containerList.add(3);
+        containerList.add(Integer.MIN_VALUE); // Пограничное значение для проверки хэш-функции
+
+        System.out.println("Size: " + containerList.size());
+
+        // Поиск элементов
+        System.out.println("Contains 1: " + containerList.contains(1));
+        System.out.println("Contains 7: " + containerList.contains(7));
+        System.out.println("Contains MIN_VALUE: " + containerList.contains(Integer.MIN_VALUE));
+
+        // Удаление элементов
+        containerList.remove(1);
+        containerList.remove(Integer.MIN_VALUE);
+        System.out.println("Contains 1 after remove: " + containerList.contains(1));
+        System.out.println("Contains MIN_VALUE after remove: " + containerList.contains(Integer.MIN_VALUE));
+        System.out.println("Size after remove: " + containerList.size());
+
+        // Очистка
+        containerList.clear();
+        System.out.println("Size after clear: " + containerList.size());
     }
 }
